4307d905b4c570e806a97af78eb2c799495eda46
Farhad hsni <farhad.hsni@gmail.com>
2016-07-15 22:26:31 +0430

    Add babel task, modify test task to run babel first

---
+++ src/js/script-complied.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function () {
+  function add(c, d) {
+    return c + d;
+  }
+  function minus(f, g) {
+    /* subracts two numbers */
+    return f - g;
+  }
+
+  module.exports = {
+    add: add,
+    minus: minus
+  };
+})();
--- test/test.js
+++ test/test.js
@@ -1,21 +1,27 @@
 /* eslint-disable global-require */
 (() => {
-  const assert = require("chai").assert;
-  const script = require("../src/js/script.js");
-
+  // const assert = require("chai").assert;
+  const script = require("../src/js/script-complied.js");
+  function assertEqual(actual, expected) {
+    if (actual !== expected) {
+      throw new Error(`expected ${expected}, but was ${actual}`);
+    }
+  }
   describe("Addition", () => {
     it("adds positive numbers", () => {
-      assert.equal(script.add(3, 4), 3);
+      // assert.equal(script.add(3, 4), 7);
+      assertEqual(script.add(3, 4), 7);
     });
 
     it("uses IEEE 754 floating point", () => {
-      assert.equal(script.add(0.1, 0.2), 0.30000000000000004);
+      // assert.equal(script.add(0.1, 0.2), 0.30000000000000004);
+      assertEqual(script.add(0.1, 0.2), 0.30000000000000004);
     });
   });
 
   describe("Subtraction", () => {
     it("subtracts positive numbers", () => {
-      assert.equal(script.minus(6, 3), 3);
+      assertEqual(script.minus(6, 3), 3);
     });
   });
 })();
